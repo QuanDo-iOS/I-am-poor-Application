be905019ae60c7deaafdf238971dc60f611748ab
QuanDoCoder <dracolucius05032002@gmail.com>
2021-12-16 11:42:27 +0700

    first commit

---
+++ Application-I-am-poor/Application-I-am-poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Application-I-am-poor
+//
+//  Created by Quan's Macbook on 06/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
